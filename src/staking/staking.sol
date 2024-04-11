0eacb06313920d81f27d106c5f2c89a12aa43449
YaelTait <yael74902@gmail.com>
2024-04-11 09:42:47 +0300

    Committing changes before merging

--- src/staking/staking.sol
+++ src/staking/staking.sol
@@ -1,82 +1,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "@hack/like/IERC20.sol";
-contract DepositAndReward {
-    IERC20 public immutable erc20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "forge-std/console.sol";
+contract Reward_4_7 is ERC20 {
     address public owner;
-    uint private  REWARD_AMOUNT = 1000000
-    // * 10**18; // 1 million reward tokens
-    uint private constant SEVEN_DAYS_IN_SECONDS = 7 * 24 * 60 * 60;
-    uint private constant TOTAL_DEPOSITS = 0;
-    // User address => deposit information =>date=>amount
-    mapping(address => mapping(uint => uint)) public deposits;
-    // User address => timestamp of last deposit
-    //mapping(address => uint) public lastDepositTime;
-    constructor(address token) {
+    uint private REWARD_AMOUNT ; // 1 million reward tokens
+    //uint private constant SEVEN_DAYS_IN_SECONDS = 7 days;
+    uint private TOTAL_DEPOSITS = 0;
+    uint WAD = 10000000000;
+    uint PERCENT;
+    //uint DATE=1750140800;
+    // Struct to hold deposit information
+    struct Deposit {
+        uint timestamp;
+        uint amount;
+    }
+    // Mapping from user address to array of deposits
+    mapping(address => Deposit[]) private userDeposits;
+    constructor(uint reward,uint percent) ERC20("Reward_4_7", "RWD_4_7") {
+        REWARD_AMOUNT=reward;
+        PERCENT=percent;
         owner = msg.sender;
-        erc20 = IERC20(token);
-        // Transfer reward tokens to this contract
-        erc20.transferFrom(msg.sender, address(this), REWARD_AMOUNT);
+        _mint(address(this), REWARD_AMOUNT);
     }
     modifier onlyOwner() {
-        require(msg.sender == owner, "not authorized");
+        require(msg.sender == owner, "Not authorized");
         _;
     }
     function deposit(uint _amount) external {
-        require(_amount > 0, "amount = 0");
-        erc20.transferFrom(msg.sender, address(this), _amount);
-        deposits[msg.sender][block.timestamp] = _amount;
-        TOTAL_DEPOSITS+=_amount;
+        require(_amount > 0, "Amount must be greater than 0");
+        transferFrom(msg.sender, address(this), _amount);
+        userDeposits[msg.sender].push(Deposit(block.timestamp, _amount));
+        TOTAL_DEPOSITS += _amount;
     }
-function calc_rewards(uint _amount) private {
-        reward=_amount/TOTAL_DEPOSITS *REWARD_AMOUNT/10;
+    function calcRewards(uint _amount) public view returns (uint) {
+        if (TOTAL_DEPOSITS == 0) return 0;
+        uint rewardPool = (REWARD_AMOUNT * PERCENT * WAD) / 100; // PERCENT% of the reward pool
+        uint reward = (_amount * rewardPool) / TOTAL_DEPOSITS;
+        return reward;
     }
-    function withdrawWithReward(uint _amount) external {
-    require(_amount > 0, "amount = 0");
-    uint BalnceWithReward = getBalnceWithReward(msg.sender);
-    require(BalnceWithReward >= _amount, "insufficient available amount for withdrawal with rewards");
-    uint reward=calc_rewards(_amount);
-    erc20.transfer(address(this),msg.sender, _amount+reward);
-    TOTAL_DEPOSITS-=_amount;
-    REWARD_AMOUNT -=reward;
-   for (uint i = 0; i < deposits[_account].length && _amount>0; i++) {
-    tempAmount=deposits[_account][i];
-    if(tempAmount<amount){
-    amount-=tempAmount;
-    deposits[_account][i]=0;
-    //delete the element by loop!
-    }
-    else{
-         deposits[_account][i]-=tempAmount;
-         amount=0;
-    }    
-    }
-}
-        function withdrawWithoutReward(uint _amount) external {
-        }
-    function getBalnceWithReward(address _account) public view returns (uint) {
-        uint sum;
-        uint latestDate = block.timestamp - SEVEN_DAYS_IN_SECONDS;
-        // Iterate over deposits for the given account
-        for (uint i = 0; i < deposits[_account].length; i++) {
-            // Check if the deposit date is at least 7 days ago
-            if (i < latestDate) {
-                sum += deposits[_account][i];
+    function withdraw(uint _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        uint balanceWithReward = getBalanceWithReward(msg.sender);
+        require(
+            balanceWithReward >= _amount,
+            "Insufficient available amount for withdrawal with rewards"
+        );
+        uint reward = calcRewards(_amount);
+        transferFrom(address(this), msg.sender, _amount + reward);
+        REWARD_AMOUNT -= reward;
+        TOTAL_DEPOSITS -= _amount;
+        uint remainingAmount = _amount;
+        uint countZero = 0;
+        for (
+            uint i = 0;
+            i < userDeposits[msg.sender].length && remainingAmount > 0;
+            i++
+        ) {
+            if (userDeposits[msg.sender][i].amount <= remainingAmount) {
+                remainingAmount -= userDeposits[msg.sender][i].amount;
+                userDeposits[msg.sender][i].amount = 0;
+                countZero++;
             } else {
-                // If the date is less than seven days ago, exit the loop
+                userDeposits[msg.sender][i].amount -= remainingAmount;
                 break;
+            }
+        }
+        for (uint j = countZero; j < userDeposits[msg.sender].length - 1; j++) {
+            userDeposits[msg.sender][j - countZero] = userDeposits[msg.sender][
+                j
+            ];
+        }
+        for (uint j = 0; j < countZero; j++) {
+            userDeposits[msg.sender].pop();
+        }
+    }
+    function getBalanceWithReward(address _account) public view returns (uint) {
+        uint sum = 0;
+        console.log(block.timestamp);
+        console.log(7 days);
+        uint latestDate = block.timestamp - 7*24*60*60;
+        for (uint i = 0; i < userDeposits[_account].length; i++) {
+            if (i< latestDate) {
+                sum += userDeposits[_account][i].amount;
             }
         }
         return sum;
     }
+    function getTotalBalance() public view returns (uint) {
+        return balanceOf(msg.sender);
     }
-    function getTotalBalance(address _account) public view returns (uint) {
-    uint totalBalance;
-    // Iterate over all timestamps in the deposits mapping for the specified account
-    // and sum up all the deposit amounts
-    for (uint i = 0; i < deposits[_account].length; i++) {
-        totalBalance += deposits[_account][i];
+    function mint(uint ammount) public {
+        _mint(msg.sender, ammount);
     }
-    return totalBalance;
-}
 }
