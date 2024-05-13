1525ec50b0f4d584e5db5a219d382e0f175e8d8e
YaelTait <yael74902@gmail.com>
2024-05-13 09:51:05 +0300

    nft tests and code

--- src/NFT/nft.sol
+++ src/NFT/nft.sol
@@ -1,63 +1,124 @@
 pragma solidity ^0.8.0;
+
 import '../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol';
-contract NFT {
+import "./myToken.sol";
+import "forge-std/console.sol";
+
+contract NFTAuction {
     address private owner;
-    IERC721 public immutable NFT_TOKEN;
+    ERC721 public nftToken;
     uint public startingBid;
     uint public endingBid;
     uint public startingPrice;
-    struct Object {
+    uint public tokenId;
+    bool public started;
+
+    struct Bidder {
         address _address;
         uint256 amount;
     }
-    struct Object2 {
+
+    struct BidState {
         bool isExist;
         uint256 amount;
     }
-    mapping(address => Object2) public balances;
-    Object[] public maxStack;
-    constructor(address _NFT_TOKEN, uint _startingBid, uint _endingBid, uint _startingPrice) {
+
+    // bidder balances
+    mapping(address => BidState) public balances;
+    Bidder[] public maxStack;
+
+    constructor() {
         owner = msg.sender;
-        NFT_TOKEN = IERC721(_NFT_TOKEN);
-        startingBid = _startingBid;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    function start(address _nftToken, uint _endingBid, uint _startingPrice, uint _tokenId) external onlyOwner {
+        nftToken = ERC721(_nftToken);
         endingBid = _endingBid;
         startingPrice = _startingPrice;
+        tokenId = _tokenId;
+        nftToken.transferFrom(owner, address(this), tokenId);
+        started = true;
     }
-    function addToBalances(address _addr, Object2 memory obj) internal {
+
+    function addToBalances(address _addr, BidState memory obj) internal {
         if (balances[_addr].isExist) {
             payable(msg.sender).transfer(balances[_addr].amount);
         }
         balances[_addr] = obj;
     }
+
     function getAddressValue(address _addr) public view returns (bool) {
         return balances[_addr].isExist;
     }
+
     function updateMax() internal {
         while (maxStack.length > 0 && !balances[maxStack[maxStack.length - 1]._address].isExist) {
             maxStack.pop();
         }
     }
-    function addQuote(address __address, uint256 _amount) public {
-        require(block.timestamp > startingBid && block.timestamp < endingBid, "the Auction is closed");
+
+    function addQuote() public payable {
+        console.log("user", msg.sender, "amount", msg.value);
+        require(started && block.timestamp < endingBid, "the Auction is closed");
+        
         updateMax();
-        require(_amount > maxStack[maxStack.length - 1].amount, "the price is too low");
-        Object memory newObj = Object(__address, _amount);
+        if (maxStack.length > 1) {
+            console.log("zzz");
+            require(msg.value > maxStack[maxStack.length - 1].amount, "the price is too low");
+        } else {
+            require(msg.value > startingPrice, "the price is too low");
+        }
+
+        Bidder memory newObj = Bidder(address(msg.sender), msg.value);
         maxStack.push(newObj);
-        Object2 memory newObj2 = Object2(true, _amount);
-        addToBalances(__address, newObj2);
+        BidState memory newObj2 = BidState(true, msg.value);
+        
+        addToBalances(address(msg.sender), newObj2);
+        console.log("userrr", msg.sender, "amount", msg.value);
     }
-    function cancelQuote(address user) public {
-        require(block.timestamp > startingBid && block.timestamp < endingBid, "the Auction is closed");
-        require(balances[user].isExist, "you are not exist");
-        balances[user].isExist = false;
-        payable(msg.sender).transfer(balances[user].amount);
+
+    function cancelQuote() public {
+        require(started && block.timestamp < endingBid, "the Auction is closed");
+        require(balances[address(msg.sender)].isExist, "you are not exist");
+        balances[address(msg.sender)].isExist = false;
+        payable(msg.sender).transfer(balances[address(msg.sender)].amount);
     }
+
     function returnQuotes() public {
         require(block.timestamp > endingBid, "the Auction is active");
         updateMax();
-        uint maxQuote = maxStack[maxStack.length - 1].amount;
+        for (uint i = 0; i < maxStack.length - 1; i++) {
+            payable(address(maxStack[i]._address)).transfer(maxStack[i].amount);
+        }
     }
-    receive() external payable {
-        addQuote(msg.sender, msg.value);
+
+    function getMaxStack() external view returns (address, uint256) {
+        uint index = maxStack.length - 1;
+        require(index < maxStack.length, "Index out of bounds");
+        return (maxStack[index]._address, maxStack[index].amount);
+    }
+
+    function getIsExist() external view returns (bool) {
+        //require(index < maxStack.length, "Index out of bounds");
+        return (balances[msg.sender].isExist);
+    }
+
+    function end() onlyOwner external {
+        require(block.timestamp > endingBid, "the Auction is active");
+        if (maxStack.length < 1) {
+            nftToken.transferFrom(address(this), owner, tokenId);
+        } else {
+            uint amount = maxStack[maxStack.length - 1].amount;
+            payable(address(owner)).transfer(amount);
+            address bidderAddress = maxStack[maxStack.length - 1]._address;
+            nftToken.transferFrom(address(this), bidderAddress, tokenId);
+            returnQuotes();
+            started = false;
+        }
     }
 }
